0464e9f109ce18e88ea509055b3119ec2fdfec09
Igor M.S <igormartinssilva2010@hotmail.com>
2023-11-10 15:03:43 -0300

    login e check login prontos

--- main.cpp
+++ main.cpp
@@ -1,12 +1,48 @@
 #include "header.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <iostream>
+#include <string.h>
+
 
 using namespace std;
 
+
+string login_user()
+{
+    string nameUser;
+
+    cout << "digite seu nome de usuario: ";
+    cin >> nameUser;
+
+    while ( nameUser.length() < 4 )
+    {
+        cout << "\nnome de usuario curto demais, digite pelo menos 4 caracteres: ";
+        cin >> nameUser;
+    }
+    
+   
+    return nameUser;
+
+}
+
+
+
+
+
+
 int main()
 {
+    string nomeLogin;
+
+
     printf ("ola, mundo");
+    nomeLogin = login_user();
+
+    printf("\nO  nome do usuário é: %s", nomeLogin.c_str() );
+
+    cout << "Login realizado com sucesso\n";
 
     return 0;
 }
