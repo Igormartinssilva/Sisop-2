2d7516b23f2b516ad1beb33d3f9c8cdfaa31eed4
luiseduardomendes <luis.pmendes13@gmail.com>
2023-11-14 15:03:15 -0300

    data

---
+++ data.hpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+class ServerNotification{
+	public:
+		int notificationId;
+		int userPostId;
+		char message;
+		int messageSize;
+		int followersToSend;
+};
+
+class UserNotification{
+	public:
+		int userPostId;
+		int userReceiverId;
+		int notificationId;
+};
+
+class FollowersGraph{
+	private:
+		std::vector<vector<int>> graph;
+	public:
+		void follow(int followerId, int followingId);
+		void getFollowers(int userId);
+}
+
+class UsersList{
+	private:
+		int nextId;
+		std::vector<vector<UserInfo>> users;
+	public:
+		void appendUser(char username);
+		void removeUser(char username);
+}
