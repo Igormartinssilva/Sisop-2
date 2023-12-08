e46fd3c621cb71ff5e8055d0e27560c6fb247237
Igormartinssilva <92041109+Igormartinssilva@users.noreply.github.com>
2023-12-08 00:14:28 -0300

    melhorias e logica no follow

---
+++ data.cpp
@@ -0,0 +1,226 @@
+#include "header/data.hpp"
+#include <iostream>
+#include <cstdint>
+#include <arpa/inet.h>
+#include "header/data.hpp"
+#include <iostream>
+#include <cassert>
+#define MAGIC_NUMBER 0
+#include <cstdint>
+
+namespace twt {
+
+    std::vector<char> serializeMessagePayload(int senderId, const std::string& message) {
+        assert(BUFFER_SIZE >= 8); // Ensure there is enough space for the header and payload
+
+        std::vector<char> serializedData(BUFFER_SIZE, 0);
+
+        // Serialize message payload
+        uint16_t senderIdN = htons(senderId);
+        std::memcpy(serializedData.data() + 0, &senderIdN, sizeof(senderIdN));
+        std::memcpy(serializedData.data() + 2, message.c_str(), message.size());
+
+        return serializedData;
+    }
+
+    std::pair<int, std::string> deserializeMessagePayload(const std::vector<char>& data) {
+        assert(data.size() >= 8); // Ensure there is enough data to deserialize the header and payload
+
+        Packet packet = deserializePacket(data);
+
+        int senderId;
+        std::memcpy(&senderId, packet.payload, sizeof(senderId));
+        senderId = ntohs(senderId);
+
+        std::string message(packet.payload + 2, sizeof(packet.payload) - 2);
+
+        return std::make_pair(senderId, message);
+    }
+
+    std::vector<char> serializeFollowPayload(int followerId, const std::string& username) {
+        assert(BUFFER_SIZE >= 8); // Ensure there is enough space for the header and payload
+
+        std::vector<char> serializedData(BUFFER_SIZE);
+
+        // Serialize follow payload
+        uint16_t followerIdN = htons(followerId);
+        std::memcpy(serializedData.data() + 0, &followerIdN, sizeof(followerIdN));
+        std::memcpy(serializedData.data() + 2, username.c_str(), username.size());
+
+        return serializedData;
+    }
+
+    std::pair<int, std::string> deserializeFollowPayload(const std::vector<char>& data) {
+        assert(data.size() >= 8); // Ensure there is enough data to deserialize the header and payload
+
+        Packet packet = deserializePacket(data);
+
+        int followerId;
+        std::memcpy(&followerId, packet.payload, sizeof(followerId));
+        followerId = ntohs(followerId);
+
+        std::string username(packet.payload + 2, sizeof(packet.payload - 2));
+
+        return std::make_pair(followerId, username);
+    }
+
+    std::vector<char> serializeExitPayload(int accountId) {
+        assert(BUFFER_SIZE >= 6); // Ensure there is enough space for the header and payload
+
+        std::vector<char> serializedData(BUFFER_SIZE);
+
+        // Serialize exit payload
+        uint16_t accountIdN = htons(accountId);
+        std::memcpy(serializedData.data(), &accountIdN, sizeof(accountIdN));
+
+        return serializedData;
+    }
+
+    int deserializeExitPayload(const std::vector<char>& data) {
+        assert(data.size() >= 6); // Ensure there is enough data to deserialize the header and payload
+
+        Packet packet = deserializePacket(data);
+
+        int accountId;
+        std::memcpy(&accountId, packet.payload, sizeof(accountId));
+        accountId = ntohs(accountId);
+
+        return accountId;
+    }
+
+    std::vector<char> serializeLoginPayload(const std::string& username) {
+        assert(BUFFER_SIZE >= 2); // Ensure there is enough space for the header
+
+        std::vector<char> serializedData(BUFFER_SIZE);
+
+        // Serialize login payload
+        std::memcpy(serializedData.data(), username.c_str(), username.size());
+
+        return serializedData;
+    }
+
+    std::string deserializeLoginPayload(const std::vector<char>& data) {
+        assert(data.size() >= 2); // Ensure there is enough data to deserialize the header and payload
+        Packet packet = deserializePacket(data);
+
+        std::string username(packet.payload);
+
+        return username;
+    }
+
+    std::vector<char> serializePacket(const Packet &pkg) {
+        assert(BUFFER_SIZE >= 6); // Ensure there is enough space for the header
+
+        std::vector<char> serializedData(BUFFER_SIZE, 0);
+
+        // Convert to network byte order (big-endian)
+        uint16_t typeN = htons(pkg.type);
+        uint16_t seqNumN = htons(pkg.sequence_number);
+        uint16_t timestampN = htons(pkg.timestamp);
+
+        int index = 0;
+        std::memcpy(serializedData.data() + index, &typeN,      sizeof(typeN));         index += sizeof(typeN);
+        std::memcpy(serializedData.data() + index, &seqNumN,    sizeof(seqNumN));       index += sizeof(seqNumN);
+        std::memcpy(serializedData.data() + index, &timestampN, sizeof(timestampN));    index += sizeof(timestampN);
+        std::memcpy(serializedData.data() + index, pkg.payload, sizeof(pkg.payload));
+
+        return serializedData;
+    }
+
+    Packet deserializePacket(const std::vector<char> &data) {
+        assert(data.size() >= 6); // Ensure there is enough data to deserialize the header
+
+        Packet pkg;
+
+        int index = 0;
+        std::memcpy(&pkg.type,              data.data() + index, sizeof(pkg.type));             index += sizeof(pkg.type);
+        std::memcpy(&pkg.sequence_number,   data.data() + index, sizeof(pkg.sequence_number));  index += sizeof(pkg.sequence_number);
+        std::memcpy(&pkg.timestamp,         data.data() + index, sizeof(pkg.timestamp));        index += sizeof(pkg.timestamp);
+
+        // Convert back to host byte order
+        pkg.type = ntohs(pkg.type);
+        pkg.sequence_number = ntohs(pkg.sequence_number);
+        pkg.timestamp = ntohs(pkg.timestamp);
+
+        std::memcpy(pkg.payload,            data.data() + index, sizeof(pkg.payload));
+
+        return pkg;
+    }
+}
+
+
+void twt::Followers::follow(int followerId, int followingId){
+    followers[followingId].insert(followerId);
+}
+
+std::unordered_set<int> twt::Followers::getFollowers(int userId){
+    return this->followers[userId];
+}
+
+  bool twt::Followers::isFollowing(int followerId, int followingId) {
+        auto it = followers.find(followingId);
+        if (it != followers.end()) {
+            return it->second.find(followerId) != it->second.end();
+        }
+        return false;
+    }
+
+   
+
+void twt::UsersList::appendUser(std::string username){
+    users.insert({nextId, UserInfo(nextId, username)});
+    usersId.insert({username, nextId});
+    this->nextId ++;
+}
+
+int twt::UsersList::getUserId(std::string username){
+    if (usersId.find(username) != usersId.end())
+        return usersId[username];
+    else return -1;
+}
+
+void twt::UsersList::removeUser(int userId){
+    users.erase(userId);
+}
+
+int twt::UsersList::createSession(std::string username){
+    int id = this->getUserId(username);
+    if (id == -1){
+        this->appendUser(username);
+        users[id].createSession();
+        return id;
+    }
+    if (!users[id].maxSessionsReached()){
+        users[id].createSession();
+        return id;
+    }
+    return id;
+}
+
+twt::UserInfo::UserInfo(){
+    this->user.username = "";
+    this->user.userId = 0;
+    this->activeSessions = 0;
+}
+
+twt::UserInfo::UserInfo(int userId, std::string username){
+    this->user.username = username;
+    this->user.userId = userId;
+    this->activeSessions = 0;
+}
+
+int twt::UserInfo::getId(){
+    return this->user.userId;
+}
+
+std::string twt::UserInfo::getUsername(){
+    return user.username;
+}
+
+bool twt::UserInfo::maxSessionsReached(){
+    return activeSessions <= MAX_SESSIONS;
+}
+
+void twt::UserInfo::createSession(){
+    this->activeSessions ++;
+}
---
+++ data.hpp
@@ -0,0 +1,111 @@
+#ifndef DATA_HPP
+#define DATA_HPP
+
+#include <vector>
+#include <map>
+#include <string>
+#include <fstream>
+#include <cstring>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <cstdint>
+#include <sstream>
+#include "../../asserts/constraints.hpp"
+
+namespace twt{
+
+	struct Packet {
+		uint16_t type;
+		uint16_t sequence_number;
+		uint16_t timestamp;
+		char payload[MSG_SIZE];
+	};
+
+	enum PacketType : uint16_t {
+		Mensagem = 0,
+		Login,
+		Follow,
+		Exit
+	};
+
+    // Serialization and Deserialization functions for different payloads
+    std::vector<char> serializeMessagePayload(int senderId, const std::string& message);
+    std::pair<int, std::string> deserializeMessagePayload(const std::vector<char>& data);
+
+    std::vector<char> serializeFollowPayload(int followerId, const std::string& username);
+    std::pair<int, std::string> deserializeFollowPayload(const std::vector<char>& data);
+
+    std::vector<char> serializeExitPayload(int accountId);
+    int deserializeExitPayload(const std::vector<char>& data);
+
+    std::vector<char> serializeLoginPayload(const std::string& username);
+    std::string deserializeLoginPayload(const std::vector<char>& data);
+
+
+	std::vector<char> serializePacket(const twt::Packet &pkg);
+	twt::Packet deserializePacket(const std::vector<char> &data);
+
+	struct ServerNotification{
+		int notificationId;
+		int userPostId;
+		char message;
+		int messageSize;
+		int followersToSend;
+	};
+
+	struct UserNotification{
+		int userPostId;
+		int userReceiverId;
+		int notificationId;
+	};
+
+	struct User{
+		std::string username;
+		int userId;
+	};
+
+	class Followers{
+		private:
+			std::unordered_map<int, std::unordered_set<int>> followers;
+		public:
+			void follow(int followerId, int followingId);
+			std::unordered_set<int> getFollowers(int userId);
+			bool twt::Followers::isFollowing(int followerId, int followingId);
+			
+	};
+
+	class UserInfo {
+		private:
+			User user;
+			int activeSessions;
+		public:
+			UserInfo();
+			UserInfo(int userId, std::string username);
+			std::string getUsername();
+			int getId();
+			bool maxSessionsReached();
+			void createSession();
+			
+	};
+
+	class UsersList{
+		private:
+			int nextId = 1;
+			std::unordered_map<int, UserInfo> users;
+			std::unordered_map<std::string, int> usersId;
+		public:
+			int getUserId(std::string username);
+			void appendUser(std::string username);
+			void removeUser(int userId);
+			int createSession(std::string username);
+			
+	};
+
+	struct Message {
+		User sender;
+		std::string content;
+	};
+}
+
+#endif
